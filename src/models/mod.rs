416c3cfff56fcf135a96b5290b41676ab764deed
FederiKFX <40461916+FederiKFX@users.noreply.github.com>
2024-05-15 20:30:24 +0300

    Update mod.rs

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 /// Response of the currencyapi
-#[derive(Debug, Deserialize, Serialize, PartialEq, PartialOrd, Clone)]
+#[derive(Debug, Deserialize, Serialize, PartialEq, Clone)]
 pub struct DetailsResponse {
     /// Data source
     pub data: HashMap<String, f64>,
